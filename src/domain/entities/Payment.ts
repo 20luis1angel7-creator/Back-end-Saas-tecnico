08205dcc0fe01e0ea31dd2550373417036f2ba06
luis angel <20luis1angel7@gmail.com>
2026-02-19 18:22:53 -0400

    entidad payment

---
+++ src/domain/entities/Payment.ts
@@ -0,0 +1,21 @@
+import { BusinessRuleError } from "../errors/DomainErrors.js";
+
+
+
+
+export class Payment {
+    constructor(
+        public readonly id: string,
+        public readonly clientId: string,
+        public readonly invoiceId: string,
+        public readonly amount: number,
+        public readonly paymentDate: Date,
+        public readonly createdAt: Date = new Date()
+    ) {
+        if (amount <= 0) {
+            throw new BusinessRuleError("Payment amount must be greater than zero")
+        }
+    }
+
+    
+}
