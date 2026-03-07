6a222ac57e8e3905be131d97896924058eb07712
luis angel <20luis1angel7@gmail.com>
2026-03-07 17:05:42 -0400

    cree invoiceroutes

--- src/presentation/routes/invoiceRoutes.ts
+++ src/presentation/routes/invoiceRoutes.ts
@@ -1,4 +1,4 @@
-import { InvoiceController } from "../controllers/InvoiceCotroller.js";
+import { InvoiceController } from "../controllers/InvoiceController.js";
 import { Router } from "express";
 
 const router = Router()
@@ -7,10 +7,13 @@ const controller = new InvoiceController()
 // pagar factura
 router.post("/invoice/:invoiceId/pay", (req, res) => controller.payInvoice(req, res))
 // obtener facturas de un cliente
-router.get("/invoice/:clientId", (req, res) => controller.getClientInvoices(req, res))
+router.get("/clients/:clientId/invoices", (req, res) => controller.getClientInvoices(req, res))
 // obtenet por id de factura
 router.get("/invoice/:invoiceId", (req, res) => controller.getInvoiceById(req, res))
-
+// generar factura
+router.post("/invoice/generate", (req, res) => controller.generate(req, res))
+// modificar factura
+router.patch("/invoice/overdue", (req, res) => controller.updateOverdueInvoices(req, res))
 
 
 
