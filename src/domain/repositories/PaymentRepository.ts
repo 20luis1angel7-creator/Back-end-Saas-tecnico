ae723833e3c59f9bc17728a9e3f527a6616891d6
luis angel <20luis1angel7@gmail.com>
2026-03-21 17:04:49 -0400

    elimine todo de pay e invoice

--- src/domain/repositories/PaymentRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Payment } from "../entities/Payment.js";
-
-export interface PaymentRepository {
-    save(payment: Payment): Promise<void>;
-    findById(clientId: string): Promise<Payment | null>;
-    findByInvoiceId(id: string): Promise<Payment[]>;
-}
--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -3,17 +3,6 @@ import { InMemoryMaterialRepository } from "./database/repositories/InMemoryMate
 import { InMemoryMaterialUsageRepository } from "./database/repositories/InMemoryOrderMaterialUsageRepository.js";
 import { InMemoryOrderRepository } from "./database/repositories/InMemoryOrderRepository.js";
 import { InMemoryPlanRepository } from "./database/repositories/InMemoryPlanRepository.js";
-import { GenerateMonthlyInvoicesUseCase } from "../application/use-cases/invoice/GenerateMonthlyinvoicesUseCase.js";
-import { InMemoryInvoiceRepository } from "./database/repositories/InMemoryInvoiceRepository.js";
-import { UuidGenerator } from "./services/IdGenerator.js";
-import { GetClientInvoiceUseCase } from "../application/use-cases/invoice/GetClientInvoiceUseCase.js";
-import { RegisterInvoicePaymentUseCase } from "../application/use-cases/invoice/RegisterInvoicePaymentUseCase.js";
-import { RecalculateClienStatusUseCase } from "../application/use-cases/invoice/RecalculateClientStatusUseCase.js";
-import { UpdateOverdueInvoiceUseCase } from "../application/use-cases/invoice/UpdateOverdueInvoiceUseCase.js";
-import { GetInvoiceByIdUseCase } from "../application/use-cases/invoice/GetInvoiceByIdUseCase.js";
-import { InMemoryPaymentRepository } from "./database/repositories/InMemoryPaymentRepository.js";
-import { RegisterPaymentUseCase } from "../application/use-cases/payment/RegisterPaymentUseCase.js";
-import { GetPaymentByInvoiceUseCase } from "../application/use-cases/payment/GetPaymentByInvoiceUseCase.js";
 import { CreatePlanUseCase } from "../application/use-cases/plan/CreatePlanUseCase.js";
 import { GetPlanByIdUseCase } from "../application/use-cases/plan/GetPlanByIdUseCase.js";
 import { ListPlansUseCase } from "../application/use-cases/plan/ListPlansUseCase.js";
@@ -34,37 +23,8 @@ export const orderRepository = new InMemoryOrderRepository();
 export const materialRepository = new InMemoryMaterialRepository();
 export const orderMaterialUsageRepository = new InMemoryMaterialUsageRepository();
 export const planRepository = new InMemoryPlanRepository();
-export const invoiceRepository = new InMemoryInvoiceRepository();
-export const idGenerator = new UuidGenerator()
 export const expenseRepository = new InMemoryExpenseRepository()
 
-
-export const generateMonthlyInvoicesUseCase = new GenerateMonthlyInvoicesUseCase(
-    planRepository,
-    clientRepository,
-    invoiceRepository,
-    idGenerator
-)
-
-export const getClientInvoiceUseCase = new GetClientInvoiceUseCase(invoiceRepository);
-
-export const recalculateClienStatusUseCase = new RecalculateClienStatusUseCase(
-    clientRepository,
-    invoiceRepository
-)
-export const registerInvoicePaymentUseCase = new RegisterInvoicePaymentUseCase(
-    invoiceRepository,
-    recalculateClienStatusUseCase
-)
-export const updateOverdueInvoiceUseCase = new UpdateOverdueInvoiceUseCase(invoiceRepository)
-export const getInvoiceByIdUseCase = new GetInvoiceByIdUseCase(invoiceRepository)
-export const paymentRepository = new InMemoryPaymentRepository()
-export const registerPaymentUseCase = new RegisterPaymentUseCase(
-    invoiceRepository,
-    paymentRepository,
-    idGenerator
-)
-export const getPaymentByInvoiceUseCase = new GetPaymentByInvoiceUseCase(paymentRepository)
 
 // Plan usecase
 export const createPlanUseCase = new CreatePlanUseCase(planRepository)
